41532b8f3134157ccc4b6e4415305eee6561d93e
TLX3 <tdl2116@columbia.edu>
2018-10-04 13:48:27 -0400

    Update solutions

---
+++ sparseMatrix.js
@@ -0,0 +1,11 @@
+let multiply = (A, B) => {
+  let C = [...new Array(A.length)].map(row => new Array(B[0].length).fill(0));
+  for (let i = 0; i < A.length; i++) {
+      for (let j = 0; j < A[0].length; j++) {
+          for (let k = 0; k < B[0].length; k++) {
+              if (B[j][k] !== 0) C[i][k] += A[i][j] * B[j][k];
+          }
+      }
+  }
+  return C;
+};
